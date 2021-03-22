0fceb79667b58a9b33914f0a61030829a41fef92
alisonhussey <alisonhussey@gmail.com>
2021-03-22 15:31:35 -0400

    add delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,19 +6,25 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    // console.log(product);
     this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
+        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
